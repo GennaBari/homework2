9a95147176167081add225f2acaff05d6aea247b
GennaBari <barinovaevgenia84@gmail.com>
2024-07-16 15:26:30 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+course_name = 'Курс: Python'
+number_of_completed_remote_sensing = 'всего задач:'
+number_of_hours_spent = 'затрачено часов:'
+time_for_one_task = 'среднее время на выполнениe:'
+a = 12
+b = 1.5
+c = b/a
+print (course_name, number_of_completed_remote_sensing, a, number_of_hours_spent, b, time_for_one_task, c)
